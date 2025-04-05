c420cb6fabf642580474ae8b1df69d515d34e621
“caigou45” <2640119029@qq.com>
2025-04-05 21:50:00 +0800

    first commit

---
+++ build/unitree_ros_to_real/unitree_legged_msgs/CMakeFiles/unitree_legged_msgs_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "/home/caigou/catkin_ws/devel/lib/python3/dist-packages/unitree_legged_msgs/msg/_BmsCmd.py"
+  "/home/caigou/catkin_ws/devel/lib/python3/dist-packages/unitree_legged_msgs/msg/_BmsState.py"
+  "/home/caigou/catkin_ws/devel/lib/python3/dist-packages/unitree_legged_msgs/msg/_Cartesian.py"
+  "/home/caigou/catkin_ws/devel/lib/python3/dist-packages/unitree_legged_msgs/msg/_HighCmd.py"
+  "/home/caigou/catkin_ws/devel/lib/python3/dist-packages/unitree_legged_msgs/msg/_HighState.py"
+  "/home/caigou/catkin_ws/devel/lib/python3/dist-packages/unitree_legged_msgs/msg/_IMU.py"
+  "/home/caigou/catkin_ws/devel/lib/python3/dist-packages/unitree_legged_msgs/msg/_LED.py"
+  "/home/caigou/catkin_ws/devel/lib/python3/dist-packages/unitree_legged_msgs/msg/_LowCmd.py"
+  "/home/caigou/catkin_ws/devel/lib/python3/dist-packages/unitree_legged_msgs/msg/_LowState.py"
+  "/home/caigou/catkin_ws/devel/lib/python3/dist-packages/unitree_legged_msgs/msg/_MotorCmd.py"
+  "/home/caigou/catkin_ws/devel/lib/python3/dist-packages/unitree_legged_msgs/msg/_MotorState.py"
+  "/home/caigou/catkin_ws/devel/lib/python3/dist-packages/unitree_legged_msgs/msg/__init__.py"
+  "CMakeFiles/unitree_legged_msgs_generate_messages_py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/unitree_legged_msgs_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
